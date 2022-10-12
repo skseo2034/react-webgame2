cb17b4818fa60ad208e4effbfc3ef4ecb864ec89
서수교 <sk.seo@navercorp.com>
2022-10-12 09:45:47 +0900

    2022-10-12 - 스터디

--- 끝말잇기/client.jsx
+++ 끝말잇기/client.jsx
@@ -1,2 +1,8 @@
 const React = require('react');
 const ReactDom = require('react-dom');
+//import React from "react";
+//import ReactDom from 'react-dom';
+//const WordRelay = require('./WordRelay');
+import  WordRelay  from './WordRelay';
+
+ReactDom.render(<WordRelay />, document.querySelector('#root'));
--- 끝말잇기/webpack.config.js
+++ 끝말잇기/webpack.config.js
@@ -1,3 +1,20 @@
-module.exports = {
+const path = require('path');
 
+module.exports = {
+    name: 'wordrelay-setting',
+    mode: 'development', // production
+    devtool: 'eval',
+
+    /*resolve: {
+        extends: ['.js', '.jsx']
+    },*/
+
+    entry: {
+        // app: ['./client.jsx', './WordRelay.jsx'] // client.jsx 에서 WordReplay.jsx 를 불러오기 때문에 실제로는 필요없다. weppack 이 알아서 해석해 준다.
+        app: ['./client.jsx'] // 확장자를 넣기 귀찮으면 resolve 로 넣어 놓으면 된다.
+    }, // 입력
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'app.js'
+    } // 출력
 };
