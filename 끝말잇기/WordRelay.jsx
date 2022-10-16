b9625b3de8b52079255cc04b0220d3a801a74728
서수교 <sk.seo@navercorp.com>
2022-10-16 10:36:45 +0900

    2022-10-16 - 스터디

--- 끝말잇기/WordRelay.jsx
+++ 끝말잇기/WordRelay.jsx
@@ -1,16 +1,56 @@
-//const React = require('react');
-// const { Component } = React;
 import React, { Component } from 'react';
-
+// const React = require('react');
+// const { Component } = React;
 
  class WordRelay extends Component {
     state = {
-
+        word: '제로초',
+        value: '',
+        result: '',
     };
 
+     onSubmitForm = (e) => {
+         e.preventDefault();
+         console.log('aaa>>>>>>>>>>', this.state.word[this.state.word.length -1], this.state.value);
+         if (this.state.word[this.state.word.length -1] == this.state.value[0]) {
+             this.setState({
+                 result: '딩동댕',
+                 word: this.state.value,
+             });
+         } else {
+             this.setState({
+                 result: '땡!!!',
+             });
+         }
+
+         this.setState({
+             value: '',
+         });
+         this.input.focus();
+     };
+
+     onChangeInput = (e) => {
+        this.setState({value: e.target.value});
+     };
+
+     input;
+
+     onRefInput = (c) => {
+        this.input = c;
+     };
     render() {
+        return (
+            <>
+                <div>{this.state.word}</div>
+                <form onSubmit={this.onSubmitForm}>
+                    <input ref={this.onRefInput} value={this.state.value} onChange={this.onChangeInput}/>
+                    <button>입력</button>
+                </form>
+                <div>{this.state.result}</div>
+            </>
+        );
     }
 }
 
-//module.exports = WordRelay;
+// module.exports = WordRelay;
 export default WordRelay;
--- 끝말잇기/client.jsx
+++ 끝말잇기/client.jsx
@@ -1,8 +1,15 @@
-const React = require('react');
-const ReactDom = require('react-dom');
-//import React from "react";
-//import ReactDom from 'react-dom';
-//const WordRelay = require('./WordRelay');
+import React from "react";
+import { createRoot } from 'react-dom/client';
 import  WordRelay  from './WordRelay';
+// import ReactDom from 'react-dom';
+// const React = require('react');
+// const ReactDom = require('react-dom');
+// const WordRelay = require('./WordRelay');
 
-ReactDom.render(<WordRelay />, document.querySelector('#root'));
+const rootElement = document.querySelector('#root');
+const root = createRoot(rootElement);
+
+root.render(
+    <WordRelay />
+);
+//ReactDom.render(<WordRelay />, document.querySelector('#root'));
