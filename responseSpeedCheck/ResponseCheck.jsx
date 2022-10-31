2b567e2a20f6cf3be3b85d2e4e88d7bfa3f743e9
서수교 <sk.seo@navercorp.com>
2022-10-31 10:13:23 +0900

    2022-10-31 - 스터디

--- responseSpeedCheck/ResponseCheck.jsx
+++ responseSpeedCheck/ResponseCheck.jsx
@@ -1,12 +1,38 @@
 import React, {useState} from "react";
 
+
+
 const ResponseCheck = () => {
+    let timeout;
+    let startTime = 0;
+    let endTime = 0;
     const [state, setState] = useState('waiting');
     const [message, setMessage] = useState('클릭해서 시작해 주세요');
-    const [result, setResult] = useState([]);
+    const [result, setResult] = useState([0]);
 
     const onClickScreen = () => {
 
+        if (state === 'waiting') {
+            setState('ready');
+            setMessage('초록색이 되면 클릭 하세요.');
+
+            timeout = setTimeout(() => {
+                setState('now');
+                setMessage('지금 클릭');
+                startTime = new Date();
+            }, Math.floor(Math.random() * 1000) + 2000); // 2~3초 랜덤
+        } else if (state === 'ready') { // 성급하게 클릭
+            setState('waiting');
+            setMessage('너무 성급하시군요! 초록색이 된 후에 클릭하세요');
+            clearTimeout(timeout);
+        } else if (state === 'now') { // 반응속도 케크
+            endTime = new Date();
+            setState('waiting')
+            setMessage('클릭해서 시작하세요');
+            setResult((prevResult) => {
+                return [...prevResult, endTime - startTime];
+            });
+        }
     };
 
     const renderAverage = () => {
