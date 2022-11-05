f2fb81372c03462c50539d9f6a728e1dd9a8734b
서수교 <sk.seo@navercorp.com>
2022-11-05 10:09:10 +0900

    2022-11-05 - 스터디

--- 가위바위보/RSP.jsx
+++ 가위바위보/RSP.jsx
@@ -1,19 +1,45 @@
 import React, {useEffect, useRef, useState} from "react";
 
 
+const rspCoords = {
+    바위: '0',
+    가위: '-142px',
+    보: '-284px',
+};
+
+const score = {
+    바위: 1,
+    가위: 0,
+    보: -1,
+};
+
 const RSP = () => {
     const [result, setResult] = useState('');
     const [score, setScore] = useState(0);
     const [imgCoord, seImgCoord] = useState(0);
 
     useEffect(() => { // 랜더링 후 실행, JSP 기준 document ready, DOMContentLoaded, 리액트 class componentDidMount()
-        fetch("https://jsonplaceholder.typicode.com/users")
-            .then((response) => response.json())
-            .then((users) => {
-                setUsers(users);
-                setLoading(false);
-            });
-    }, []); // 랜더링 직후 함번만 실행 위해 빈배열 넣음. [result] 를 넣으면 setResult 될때 마다 실행(class componentDidUpdate )
+        const interval = setInterval(() => {
+            if (imgCoord === rspCoords.바위) {
+                seImgCoord(rspCoords.가위);
+            } else if (imgCoord === rspCoords.가위) {
+                seImgCoord(rspCoords.보);
+            } else {
+                seImgCoord(rspCoords.바위);
+            }
+        }, 1000);
+        return () => {
+            window.clearInterval(interval);
+        };
+        //return () => clearInterval(interval);
+       //clearInterval(interval);
+    }, [imgCoord]); // 랜더링 직후 함번만 실행 위해 빈배열 넣음. [result] 를 넣으면 setResult 될때 마다 실행(class componentDidUpdate )
+
+    /*useEffect(() => {
+        window.addEventListener("interval", setInterval);
+       return () => window.removeEventListener("interval", setInterval);
+        // return () => clearInterval(interval);
+    }, []);*/
 
     // componentWillUnmount 예시
    /* useEffect(() => {
@@ -22,14 +48,16 @@ const RSP = () => {
         window.addEventListener("scroll", onScroll);
         return () => window.removeEventListener("scroll", onScroll); <---- 집중 !!!
     }, []);*/
-
+    const onClickBtn = (aaa) => {
+        //...
+    }
     return (
         <>
-            <div id"computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}} />
+            <div id="computer" style={{ background: `url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ${imgCoord} 0`}} />
             <div>
-                <button id-"rock" className="btn" onClick={() => onClickBtn('바위')}>바위</button>
-                <button id-"scissor" className="btn" onClick={() => onClickBtn('가위')}>바위</button>
-                <button id-"paper" className="btn" onClick={() => onClickBtn('보')}>바위</button>
+                <button id="rock" className="btn" onClick={() => onClickBtn('바위')}>바위</button>
+                <button id="scissor" className="btn" onClick={() => onClickBtn('가위')}>가위</button>
+                <button id="paper" className="btn" onClick={() => onClickBtn('보')}>보</button>
             </div>
             <div>{result}</div>
             <div>현재 {score}점</div>
