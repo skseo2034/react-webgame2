b4ee91a60ab030656a7eb77adcf36f8ebac8153d
서수교 <sk.seo@navercorp.com>
2022-11-22 12:18:43 +0900

    2022-11-21 - 스터디

--- 7.틱택토/Td.jsx
+++ 7.틱택토/Td.jsx
@@ -1,7 +1,15 @@
-import React, { useCallback } from "react";
+import React, {memo, useCallback, useEffect, useRef} from "react";
 import { CLICK_CELL, CHANGE_TURN } from "./TicTacToe";
 
-const Td = ({rowIndex, cellIndex, dispatch, cellData}) => {
+const Td = memo({rowIndex, cellIndex, dispatch, cellData}) => {
+    console.log('td rendered');
+
+    const ref = useRef([]);
+    useEffect(() => {
+        console.log(rowIndex === ref.current[0], cellIndex === ref.current[1], dispatch === ref.current[2], cellData === ref.current[3]);
+        ref.current = [rowIndex, cellIndex, dispatch, cellData];
+    }, [rowIndex, cellIndex, dispatch, cellData]); // 무엇 때문에 리랜더링 되는지 확인하기 위해 각종 props 를 넣는다.
+
     const onClickTd = useCallback(() => {
         console.log(rowIndex, cellIndex);
         if (cellData) {
@@ -9,7 +17,7 @@ const Td = ({rowIndex, cellIndex, dispatch, cellData}) => {
         }
         dispatch({ type: CLICK_CELL, row: rowIndex, cell: cellIndex});
 
-    }, [cellData]);
+    }, [cellData]); // 바뀔것 같은 데이터를 넣어 주면 된다.
 
     return (
         <td onClick={onClickTd}>{cellData}</td>
--- 7.틱택토/Tr.jsx
+++ 7.틱택토/Tr.jsx
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {memo, useEffect, useMemo, useRef} from "react";
 import Td from "./Td";
 
-const Tr = ({ rowIndex, rowData, dispatch }) => {
+const Tr = memo({ rowIndex, rowData, dispatch }) => {
+    console.log('tr rendered');
+
+    const ref = useRef([]);
+    useEffect(() => {
+        console.log(rowIndex === ref.current[0], rowData === ref.current[1], dispatch === ref.current[2]);
+        ref.current = [rowIndex, rowData, dispatch];
+    }, [rowIndex, rowData, dispatch]);
+
     return (
         <tr>
             <Td dispatch={dispatch}/>
-            {Array(rowData.length).fill().map((td, i) => (<Td fowIndex={rowIndex} cellIndex={i} cellData={rowData[i]}>{''}</Td>))}
+            {Array(rowData.length).fill().map((td, i) => (
+                useMemo( // 컴포넌트를 기억한다.
+                    () => <Td key={i} fowIndex={rowIndex} cellIndex={i} cellData={rowData[i]}>{''}</Td>,
+                    [rowData[i]],
+                )
+            ))}
         </tr>
     );
 };
