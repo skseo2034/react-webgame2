8739af7883f45973265e669b2f404676c4e9b9f4
서수교 <sk.seo@navercorp.com>
2022-11-25 11:00:26 +0900

    2022-11-25 - 스터디

--- 8.지뢰찾기/MineSearch.jsx
+++ 8.지뢰찾기/MineSearch.jsx
@@ -32,9 +32,35 @@ const init = initialState => {
 
 export const START_GAME = 'START_GAME';
 
-function plantMine(row, cell, mine) {
+const plantMine = (row, cell, mine) => {
+    console.log(row, cell, mine);
+    const candidate = Array(row * cell).fill().map((arr, i) => {
+        return i;
+    });
+    const shuffle = [];
+    while (candidate.length > row * cell - mine) {
+        const chosen = candidate.splice(Math.floor(Math.random() *  candidate.length), 1)[0];
+        shuffle.push(chosen);
+    }
 
-}
+    const data = [];
+    for (let i = 0; i < row; i++) {
+        const rowData = [];
+        data.push(rowData);
+        for (let j = 0; j < cell; j++) {
+            rowData.push(CODE.NORMAL);
+        }
+    }
+
+    for (let k = 0; k < shuffle.length; k++) {
+        const ver = Math.floor(shuffle[k] / cell);
+        const hor = shuffle[k] % cell;
+        data[ver][hor] = CODE.MINE;
+    }
+
+    console.log(data);
+    return data;
+};
 
 const reducer = (state, action) => {
     switch (action.type) {
--- 8.지뢰찾기/Table.jsx
+++ 8.지뢰찾기/Table.jsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import Tr from "./Tr";
+import { TableContext } from "./MineSearch";
 
 const Table = () => {
+    const { tableData } = useContext(TableContext);
+    return (
+        <table>
+            {Array().fill().map((tr, i) => <Tr rowIndex={i}/>)}
+        </table>
 
+    )
 }
 
 export default Table;
